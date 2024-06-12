beebecd8d7cbf19a5c0d11a8d51ec2ce34af9f66
Steve07r6 <townsens4599@student.faytechcc.edu>
2024-06-12 19:29:38 -0400

    Add files via upload
    First Submission

---
+++ P1LAB1_TownsendStephen.py
@@ -0,0 +1,13 @@
+# Stephen Townsend
+# June 12th 2024
+# P1LAB1
+# This is a basic code text for an introduction to Python
+
+# Prompting the user to enter their first name
+first_name = input("Enter your first name: ")
+
+# Prompting the user to enter their last name
+last_name = input("Enter your last name: ")
+
+# Displaying a welcome message
+print("Welcome,", first_name, last_name, "to this course!")
